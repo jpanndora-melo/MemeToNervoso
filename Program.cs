4e788aea8a380d95bbcd1bf7fbb4d42480267e75
Jessica <jpanndora@gmail.com>
2025-11-01 21:09:53 -0300

    Atividade concluida

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string fraseDigitada, fraseMemeToNervoso;
+
+Console.Write("Usuário, o que voce pensa a me respeito? ");
+fraseDigitada = Console.ReadLine()!;
+
+fraseMemeToNervoso = $"{fraseDigitada.Remove(10)} calma, espera ...To nervoso!";
+Console.WriteLine(fraseMemeToNervoso);
+
+
